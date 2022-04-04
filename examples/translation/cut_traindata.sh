e1bb8354a34279c4d6968a03f9c5f22b176b3424
Byungwook Kang <byungwook@ByungWook-M1-pro.local>
2022-04-04 20:10:04 +0900

    Fix cutting script

--- examples/translation/cut_traindata.sh
+++ examples/translation/cut_traindata.sh
@@ -1,15 +1,14 @@
+set -x
+
 SCRIPT_PATH=$(dirname $0)
-LINES=${1:-10000}
-TEXT_DIR=${2:-$SCRIPT_PATH/iwslt14.tokenized.de-en}
+TEXT_DIR=${1:-$SCRIPT_PATH/iwslt14.tokenized.de-en}
+LINES=${2:-160239}
 
-# copy train data
-if [ ! -f $TEXT_DIR/train-backup.de ]; then
-    cp $TEXT_DIR/train.de $TEXT_DIR/train-backup.de
-fi
-echo "1,${LINES}p"
-sed -n "1,${LINES}p" $TEXT_DIR/train-backup.de >$TEXT_DIR/train.de
+echo "Cutting training data..."
+echo "length of training data: $LINES"
 
-if [ ! -f $TEXT_DIR/train-backup.en ]; then
-    cp $TEXT_DIR/train.en $TEXT_DIR/train-backup.en
-fi
-sed -n "1,${LINES}p" $TEXT_DIR/train-backup.en >$TEXT_DIR/train.en
+sed -n "1,${LINES}p" $TEXT_DIR/train.de >$TEXT_DIR/train-cut.de
+sed -n "1,${LINES}p" $TEXT_DIR/train.en >$TEXT_DIR/train-cut.en
+
+set +x
+echo "Done."
